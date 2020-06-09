00613c348d1303c1e64d0773d7810a408cc6c717
Archit Rathi <64769555+arath089@users.noreply.github.com>
2020-06-09 15:01:14 -0600

    Add files via upload

---
+++ CheynesStokesSimulation.m
@@ -0,0 +1,42 @@
+%% Cheynes-Stokes Simulation
+% Author: Zach Baird
+% Date: May 28, 2018
+% University of Ottawa & Carleton University
+% This program generates a signal which represents Cheynes-Stokes breathing
+% patterns
+
+%% Parameters
+fs=256; % Sampling Rate
+br=15; % Breathing Rate
+cycle=50; % Cheyne-Stokes cyle time in secods
+apnea=10; % Apnea length
+time=10; % Total time in minutes
+VarEnv=0.1; % Variance of envelope noise
+VarPhase=0.1; % Variance of phase noise in breathing signal
+
+%% Generate Signal
+t=linspace(0,time*60, time*60*fs); % Time vector
+Sn=cos(2*pi*(br/60).*t+VarPhase*randn(1,time*60*fs)); % Normal Breathing signal
+% Construct hyperpnea/apnea window function 
+win=blackman(cycle*fs)'; % crescendo-diminuendo pattern represented by Blackman window
+apneawin=zeros(1,apnea*fs); % apnea window represented by zeros
+envelope=[win,apneawin]; % One period of envelope cycle
+% Periodically extend window function
+while length(envelope)<length(Sn)
+    envelope=[envelope,envelope];
+end
+envelope=envelope(1:length(Sn))+VarEnv.*randn(1,time*60*fs); % Trim length of window to match signal length
+CSB=Sn.*envelope; % Cheynes Stokes breathing pattern
+
+
+%% Plot Signals
+subplot(2,1,1)
+plot(t/60,Sn)
+title('Normal Breathing')
+xlabel('time (min)')
+ylabel ('amplitude')
+subplot(2,1,2)
+plot(t/60,CSB)
+title('Cheyne-Stokes Breathing')
+xlabel('time (min)')
+ylabel ('amplitude')
